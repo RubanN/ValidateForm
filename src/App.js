3cd80b0247e63657c8e261e4dc54799b78b700aa
Ruban <Ruban.Viswanathan@infovision.com>
2021-04-02 23:41:02 +0530

    second props commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from "react";
+import {useState} from "react";
 import "./App.css";
-import Form from "./components/Form";
-
+import Home from './components/Home'
+import Employee from './components/Employee'
 
 const App = () => {
+    
+
   return (
-    <div className="App">
-  
-     <Form/>
+    <div className="app">
+         <Employee FirstName="ruban" lastName="V"/>
+         <Employee FirstName="jack" lastName="V"/>
+         <Employee FirstName="cricket" lastName="Y"/>
+         <Employee FirstName="google" lastName="G"/>
+         <Employee FirstName="ruban" lastName="G"/>
+         
          </div>
   );
 };
